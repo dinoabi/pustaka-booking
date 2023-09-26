cf4b42b3726e6dc7aa1974a2c66e0fd053c48dde
dinoabi <dinoabi039@gmail.com>
2023-09-26 15:51:15 +0700

    tugas biodata pertemuan 2

---
+++ application/views/View_biodata.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Nama : Dino Abi Nagari</h1>
+        <h1>Kelas : 10.3A.07</h1>
+        <h1>Jurusan : RPL</h1>
+        <h1>Alamat : Jl.Sunter Jaya VI A, Jakarta Utara</h1>
+        <h1>No Hp : 083896662257</h1>
+    </div>
+</body>
+</html>
